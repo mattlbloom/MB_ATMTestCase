7290467ddd87e77cd178f62bf2a12fe1f1639e7b
Matthew Bloom <mbloom@elon.edu>
2015-09-07 16:03:11 -0400

    Initial commit

---
+++ src/edu/elon/test/ATMTest.java
@@ -0,0 +1,84 @@
+/**
+ * ATMTest.java 1.0 Sep 7, 2015
+ * 
+ * Copyright (c) 2015 Matthew Bloom. All Rights Reserved
+ */
+package edu.elon.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Enter summary here
+ * @author Matthew Bloom
+ * @version 1.0
+ *
+ */
+public class ATMTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#ATM()}.
+   */
+  @Test
+  public void testATM() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#ATM(double)}.
+   */
+  @Test
+  public void testATMDouble() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#deposit(double)}.
+   */
+  @Test
+  public void testDeposit() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#getBalance()}.
+   */
+  @Test
+  public void testGetBalance() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#toString()}.
+   */
+  @Test
+  public void testToString() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link edu.elon.test.ATM#withdraw(double)}.
+   */
+  @Test
+  public void testWithdraw() {
+    fail("Not yet implemented");
+  }
+
+}
